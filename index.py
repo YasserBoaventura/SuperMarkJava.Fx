be2e7c357c5c60a59b678f75726aa803b1c07e22
Yasser Boaventura <yasserboaventura78@gmail.com>
2025-06-09 00:09:16 +0100

    Mantendo apenas OF.java no repositório

--- index.py
+++
@@ -1,8 +0,0 @@
-def adicionar(x, y=10):
-    return x+y
-resultado=adicionar(2,8)
-print(resultado)
-def  mutiplicar(w, r):
-    return w*r
-resultado=mutiplicar(2,3)
-print(resultado)
--- janelaOutput1.java
+++
@@ -1,6 +0,0 @@
-import javax.swing.*;
-import java.awt.Container;
-public class janelaOutput1 {
-
-    
-}
